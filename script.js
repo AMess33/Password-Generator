29ab4c54b082301c67ab16792005cf88ba3a6eed
AMess33 <amess3309@gmail.com>
2022-12-21 00:31:47 -0600

    update js

--- script.js
+++ script.js
@@ -7,24 +7,38 @@
 // generated password is visible on page
 
 function passwordLength(){
-  var characterCount = prompt("How many characters do you want to use in your password? Please enter a number between 8-128");
+  prompt("How many characters do you want to use in your password? Please enter a number between 8-128", 12);
 }
 
 function lowerCaseCharacter(){
-  var includeLower = prompt("Do you want to include lower case letters in your password? Type 'yes' or 'no'")
+  prompt("Do you want to include lower case letters in your password? Type 'yes' or 'no'", 'yes')
 }
 
 function upperCaseCharacter(){
-  var includeUpper = prompt("Do you want to include Upper case letters in your password? Type 'yes' or 'no'")
+  prompt("Do you want to include Upper case letters in your password? Type 'yes' or 'no'", 'yes')
 }
 
 function numberCharacter(){
-  var includeNumber = prompt("Do you want to include numbers in your password? Type 'yes' or 'no'")
+  prompt("Do you want to include numbers in your password? Type 'yes' or 'no'", 'yes')
 }
 
 function specialCharacter(){
-  var includeSpecial = prompt("Do you want to include spcecial characters in your password? Type 'yes' or 'no'")
+  prompt("Do you want to include spcecial characters in your password? Type 'yes' or 'no'", 'yes')
 }
+
+// Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
 var btn = document.getElementById("generate");
 btn.addEventListener('click', Event => {
@@ -34,18 +48,3 @@ btn.addEventListener('click', Event => {
   numberCharacter();
   specialCharacter();
 });
-
-// Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
